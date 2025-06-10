8fdfc699b15c7274c79a1509723e3667c326de74
uFNet1 <91597674+uFNet1@users.noreply.github.com>
2025-06-10 09:43:13 +0300

    add buttons to control state of color and opacity

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const root = document.querySelector("#app");
 const blocksContainer = document.querySelector("#blocks-container");
 const changeSizeBtn = document.querySelector("#change-size");
+const changeColorBtn = document.querySelector("#change-color");
+const changeOpacityBtn = document.querySelector("#change-opacity");
+
+let currentOpacity = 0;
 function initialize() {
   changeSizeBtn.onclick = () => {
     changeGrid();
   };
+  changeColorBtn.onclick = () => {
+    if (changeColorBtn.textContent === "Red") {
+      changeColorBtn.textContent = "Random color";
+    } else {
+      changeColorBtn.textContent = "Red";
+    }
+  };
+  changeOpacityBtn.onclick = () => {
+    if (changeOpacityBtn.textContent === "Fully opaque") {
+      changeOpacityBtn.textContent = "Progressive";
+      currentOpacity = 0;
+    } else changeOpacityBtn.textContent = "Fully opaque";
+  };
+  changeSizeBtn.textContent = "16x16";
+  changeColorBtn.textContent = "Random color";
+  changeOpacityBtn.textContent = "Fully opaque";
+
   generateGrid(16);
 }
 function generateGrid(size) {
@@ -16,7 +37,17 @@ function generateGrid(size) {
       block.className = "block";
       block.addEventListener("mouseover", () => {
         if (block.matches(":hover")) {
-          block.classList.add("painted");
+          //
+          if (changeColorBtn.textContent === "Random color") {
+            block.style.backgroundColor = getRandomColor();
+          } else if (changeColorBtn.textContent === "Red") {
+            block.style.backgroundColor = `rgb(255, 0, 0)`;
+          }
+
+          if (changeOpacityBtn.textContent === "Progressive") {
+            block.style.opacity = `${currentOpacity}%`;
+            if (currentOpacity < 100) currentOpacity += 10;
+          }
         }
       });
 
@@ -35,4 +66,13 @@ function changeGrid() {
   }
   blocksContainer.innerHTML = "";
   generateGrid(Number(size));
+  changeSizeBtn.textContent = `${size}x${size}`;
+}
+
+function getRandomColor() {
+  function getRandom() {
+    return Math.floor(Math.random() * 256);
+  }
+
+  return `rgb(${getRandom()}, ${getRandom()}, ${getRandom()})`;
 }
